c46c4f19e41570a6ac5b577cb39c8612322282f9
Shivakrishna_Kurremu <itzshivakrishna@gmail.com>
2019-11-22 16:03:04 +0530

    hackerrank - warm up

--- src/main/java/basic/DiagonalDifference.java
+++ src/main/java/basic/DiagonalDifference.java
@@ -1,56 +1,33 @@
-/*
 package basic;
 
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 class DiagonalDifference {
 
-    */
-/*
+    /*
      * Complete the 'diagonalDifference' function below.
      *
      * The function is expected to return an INTEGER.
      * The function accepts 2D_INTEGER_ARRAY arr as parameter.
-     *//*
-
+     */
 
     public static int diagonalDifference(List<List<Integer>> arr) {
-    // Write your code here
-
-    }
-
-}
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<List<Integer>> arr = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-            List<Integer> arrRowItems = new ArrayList<>();
-
-            for (int j = 0; j < n; j++) {
-                int arrItem = Integer.parseInt(arrRowTempItems[j]);
-                arrRowItems.add(arrItem);
-            }
-
-            arr.add(arrRowItems);
+        //00 01 02 03
+        //10 11 12 13
+        //20 21 22 23
+        //30 31 32 33
+        int firstSum = 0, lastSum = 0, k = arr.size();
+        for (int i = 0; i < arr.size(); i++) {
+            firstSum += arr.get(i).get(i);
+            lastSum += arr.get(i).get(k - 1 - i);
         }
-
-        int result = DiagonalDifference.diagonalDifference(arr);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        return Math.abs(firstSum - lastSum);
     }
+
 }
-*/
