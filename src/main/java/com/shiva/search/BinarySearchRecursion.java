899f4268a9e934746ad45257cd2d331997d8278d
Shivakrishna Kurremula <shivakrishna_kurremula@epam.com>
2020-03-11 09:48:52 +0530

    search algos

--- src/main/java/com/shiva/search/BinarySearchRecursion.java
+++ src/main/java/com/shiva/search/BinarySearchRecursion.java
@@ -3,31 +3,30 @@ package com.shiva.search;
 public class BinarySearchRecursion {
     public static void main(String[] args) {
         int[] arr = {1, 4, 5, 10, 33, 45, 65, 75, 79, 80, 94, 99};
-        int n = 79;
-        int index = binarySearch(arr, n);
+        int key = 79;
+        int left = 0;
+        int right = arr.length - 1;
+        int index = binarySearch(arr, left, right,  key);
         System.out.println("Found key at index " + index);
     }
 
     //if n > mid - l = mid and high = max
     //if n< mid - l = 0  and high = mid
-    private static int binarySearch(int[] arr, int key) {
-        int low = 0;
-        int high = arr.length - 1;
-        while (low <= high) {
-            System.out.println("low " + low);
-            System.out.println("high " + high);
-            int mid =  (high + low) / 2;
-            System.out.println("mid " + mid);
-            if (key < arr[mid]) {
-                high = mid - 1;
-                System.out.println("highhhhhh ");
-            } else if (key > arr[mid]) {
-                low = mid + 1;
-                System.out.println("loowwww ");
-            } else {
-                return mid;
-            }
-        }
-        return -1;
+    private static int binarySearch(int[] arr, int left, int right, int key) {
+    	if(left <= right) {
+    		int mid = (left + right) / 2;
+    		System.out.println("mid " + mid );
+    		if (arr[mid] == key) {
+				return mid;
+			} 
+    		if (arr[mid] > key) {
+				System.out.println("left " + mid );
+				return binarySearch(arr, left, mid - 1, key);
+			} 
+		
+			System.out.println("right " + mid );
+			return binarySearch(arr, mid + 1, right, key);
+    	}
+    	return -1;
     }
 }
--- src/main/java/com/shiva/search/JumpSearch.java
+++ src/main/java/com/shiva/search/JumpSearch.java
@@ -1,16 +1,30 @@
 package com.shiva.search;
 
 public class JumpSearch {
-	 public static void main(String[] args) {
-	        int[] arr = {1, 4, 5, 10, 33, 45, 65, 75, 79, 80, 94, 99};
-	        int n = 79;
-	        int index = search(arr, n);
-	        System.out.println("Found key at index " + index);
-	    }
+	public static void main(String[] args) {
+		int[] arr = { 1, 4, 5, 10, 33, 45, 65, 75, 79, 80, 94, 99 };
+		int key = 65;
+		int size = arr.length;
+		int index = search(arr, size, key);
+		System.out.println("Found key at index " + index);
+	}
 
-	private static int search(int[] arr, int n) {
-		for (int i = 0; i < arr.length; i++) {
-			
+	private static int search(int[] arr, int size, int key) {
+		int step = (int) Math.floor(Math.sqrt(size));
+		int prev = 0;
+		// Finding the Block
+		while (arr[Math.min(step, size) - 1] < key) {
+			prev = step;
+			step += (int) Math.floor(Math.sqrt(size));
+			if (prev >= size) {
+				return -1;
+			}
+		}
+		while (prev < step) {
+			if (arr[prev] == key) {
+				return prev;
+			}
+			prev++;
 		}
 		return -1;
 	}
