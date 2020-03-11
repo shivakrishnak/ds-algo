ce4b1bd4057a4871de07de7985270e19a0c76e1e
Shivakrishna Kurremula <shivakrishna_kurremula@epam.com>
2020-03-11 10:37:34 +0530

    search with recursion algos

--- src/main/java/com/shiva/search/LinearSearch.java
+++ src/main/java/com/shiva/search/LinearSearch.java
@@ -1,16 +1,17 @@
 package com.shiva.search;
 
 public class LinearSearch {
-	 public static void main(String[] args) {
-	        int[] arr = {1, 4, 5, 10, 33, 45, 65, 75, 79, 80, 94, 99};
-	        int n = 79;
-	        int index = search(arr, n);
-	        System.out.println("Found key at index " + index);
-	    }
+	public static void main(String[] args) {
+		int[] arr = { 1, 4, 5, 10, 33, 45, 65, 75, 79, 80, 94, 99 };
+		int key = 79;
+		int size = arr.length;
+		int index = search(arr, size, key);
+		System.out.println("Found key at index " + index);
+	}
 
-	private static int search(int[] arr, int n) {
-		for (int i = 0; i < arr.length; i++) {
-			if( n == arr[i]) {
+	private static int search(int[] arr, int size, int key) {
+		for (int i = 0; i < size; i++) {
+			if (key == arr[i]) {
 				return i;
 			}
 		}
