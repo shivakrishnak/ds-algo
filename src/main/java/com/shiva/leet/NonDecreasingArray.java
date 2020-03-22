ebb1ac64a1377cf921c8f198e427d79edbac7323
Shivakrishna_Kurremu <Shivakrishna_Kurremu@EPINHYDW023D.princeton.epam.com>
2020-03-22 20:05:32 +0530

    leet code problems

---
+++ src/main/java/com/shiva/leet/NonDecreasingArray.java
@@ -0,0 +1,25 @@
+package com.shiva.leet;
+
+//https://leetcode.com/problems/non-decreasing-array/
+public class NonDecreasingArray {
+	public static void main(String[] args) {
+		NonDecreasingArray nd = new NonDecreasingArray();
+		System.out.println(nd.checkPossibility(new int[] { 1, 5, 2, 4, 3 }));
+	}
+
+	public boolean checkPossibility(int[] nums) {
+		int count = 0;
+		int prev = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			if (prev > nums[i]) {
+				if (count++ > 0)
+					return false;
+				if (i - 2 >= 0 && nums[i - 2] > nums[i])
+					continue;
+			}
+			prev = nums[i];
+		}
+		return true;
+	}
+
+}
