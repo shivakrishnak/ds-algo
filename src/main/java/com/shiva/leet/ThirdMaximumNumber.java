1dbb9d96e78e5d6111dc35e962726f92cd2409da
Shivakrishna_Kurremu <Shivakrishna_Kurremu@EPINHYDW023D.princeton.epam.com>
2020-03-28 21:03:31 +0530

    leet algos

---
+++ src/main/java/com/shiva/leet/ThirdMaximumNumber.java
@@ -0,0 +1,32 @@
+package com.shiva.leet;
+
+import java.util.Arrays;
+
+public class ThirdMaximumNumber {
+	public static void main(String[] args) {
+		ThirdMaximumNumber max = new ThirdMaximumNumber();
+		System.out.println(max.thirdMax(new int[]{1,2,-2147483648}));
+	}
+	public int thirdMax(int[] nums) {
+		Integer m1 = null;
+		Integer m2 = null;
+		Integer m3 = null;
+		for (Integer n : nums) {
+			if (n.equals(m1) || n.equals(m2) || n.equals(m3)) continue;
+			if (m1 == null || n > m1) {
+				m3 = m2;
+				m2 = m1;
+				m1 = n;
+			}
+			else if(m2 == null || (n < m1 && n > m2)) {
+				m3 = m2;
+				m2 = n;
+			}
+			else if(m3 == null || (n < m1 && n < m2 && n > m3)) {
+				m3 = n;
+			}
+		}
+		System.out.println("m1 : " + m1 + " m2 : " + m2 + " m3 : " + m3);
+		return m3 == null ? m1 : m3;
+	}
+}
