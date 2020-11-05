ed959453777bacb2aeb1c218af0852bb1b038bd0
Shiva Krishna <itzshivakrishna@gmail.com>
2020-11-05 17:43:47 +0530

    algos

--- src/main/java/com/shiva/dcp/MissingPositiveInteger.java
+++ src/main/java/com/shiva/dcp/MissingPositiveInteger.java
@@ -1,5 +1,6 @@
 package com.shiva.dcp;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,6 +12,33 @@ public class MissingPositiveInteger {
     }
 
     private static int find(int[] arr) {
+        if (arr.length == 0) {
+            return 1;
+        }
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[arr[i] - 1] != arr[i]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+        i = 0;
+        while (i < arr.length && arr[i] == i + 1) {
+            i++;
+        }
+
+        System.out.println(Arrays.toString(arr));
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static int find1(int[] arr) {
         Set set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
@@ -23,6 +51,6 @@ public class MissingPositiveInteger {
             }
         }
         System.out.println(set);
-        return 0;
+        return 1;
     }
 }
