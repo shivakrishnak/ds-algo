7fb4ca2444918d83cd4f9fb18b4f2f7c7d9ea6c2
Shiva Krishna <itzshivakrishna@gmail.com>
2020-11-07 19:33:45 +0530

    algos

--- src/main/java/com/shiva/dcp/NonAdjacentLargestSum.java
+++ src/main/java/com/shiva/dcp/NonAdjacentLargestSum.java
@@ -6,6 +6,7 @@ public class NonAdjacentLargestSum {
     public static void main(String[] args) {
         int[] arr = new int[]{5, 1, 1, 5};
         System.out.println(find(arr));
+        System.out.println(check(arr));
     }
 
     private static int find(int[] arr) {
@@ -17,6 +18,17 @@ public class NonAdjacentLargestSum {
             dp[0] = Math.max(dp[0], temp);
 
         }
-        return Math.max(dp[0],dp[1]);
+        return Math.max(dp[0], dp[1]);
+    }
+
+    private static int check(int[] arr) {
+        int excl = 0;
+        int incl = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int temp = incl;
+            incl = Math.max(excl + arr[i], incl);
+            excl = temp;
+        }
+        return incl;
     }
 }
