2d63198471f77df098f31ca20a4e9926dd25963f
Shiva Krishna <itzshivakrishna@gmail.com>
2022-11-14 19:07:03 +0530

    20. Valid Parentheses

--- src/main/java/com/algo/leet/stack/ValidParentheses.java
+++ src/main/java/com/algo/leet/stack/ValidParentheses.java
@@ -1,10 +1,43 @@
 package com.algo.leet.stack;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 //https://leetcode.com/problems/valid-parentheses/description/
-class ValidParentheses {
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(new ValidParentheses().isValid("([}}])"));
+    }
+
     public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Map<Character, Character> openToClose = new HashMap<>();
+        openToClose.put('}', '{');
+        openToClose.put(')', '(');
+        openToClose.put(']', '[');
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (openToClose.containsKey(c)) {
+                if (stack.empty()) {
+                    return false;
+                } else {
+                    if (openToClose.get(c) == stack.peek()) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public boolean isValid1(String s) {
         if (s.length() % 2 != 0) return false;
 
         Stack<Character> stack = new Stack<>();
@@ -38,9 +71,5 @@ class ValidParentheses {
             }
         }
         return stack.isEmpty();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new ValidParentheses().isValid("}}"));
     }
 }
