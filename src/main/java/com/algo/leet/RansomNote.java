84417a5bf153d47a0518eb28ac123f4331a08158
Shiva Krishna <itzshivakrishna@gmail.com>
2022-11-15 20:55:54 +0530

    383. Ransom Note

--- src/main/java/com/algo/leet/RansomNote.java
+++ src/main/java/com/algo/leet/RansomNote.java
@@ -1,17 +1,39 @@
 package com.algo.leet;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-class RansomNote {
+//https://leetcode.com/problems/ransom-note/description/
+public class RansomNote {
     public static void main(String[] args) {
-        RansomNote rn = new RansomNote();
-        System.out.println(rn.canConstruct("a", "b"));
-        System.out.println(rn.canConstruct("aa", "ab"));
-        System.out.println(rn.canConstruct("aa", "aab"));
-        System.out.println(rn.canConstruct("aab", "baa"));
-
+        //System.out.println(new RansomNote().canConstruct("aa", "ab"));
+        System.out.println(new RansomNote().canConstruct("aa", "aab"));
+        //System.out.println(new RansomNote().canConstruct("fihjjjjei", "hjibagacbhadfaefdjaeaebgi"));
     }
+
     public boolean canConstruct(String ransomNote, String magazine) {
-        return Arrays.asList(magazine.toCharArray()).containsAll(Arrays.asList(ransomNote.toCharArray()));
+        Map<Character, Integer> mags = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            if (mags.containsKey(magazine.charAt(i))) {
+                mags.put(magazine.charAt(i), mags.get(magazine.charAt(i)) + 1);
+            }
+            mags.computeIfAbsent(magazine.charAt(i), val -> 1);
+        }
+        System.out.println(mags);
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            Character ch = ransomNote.charAt(i);
+            if (mags.containsKey(ch)) {
+                if (mags.get(ch) == 1) {
+                    mags.remove(ch);
+                } else {
+                    mags.put(ch, mags.get(ch) - 1);
+                }
+            } else {
+                return false;
+            }
+        }
+        System.out.println(mags);
+        return true;
     }
 }
