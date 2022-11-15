0c30077f64783863782ee6ba89c76a59298d9765
Shiva Krishna <itzshivakrishna@gmail.com>
2022-11-15 16:58:30 +0530

    11. Container With Most Water

--- src/main/java/com/algo/leet/array/ContainerWithMostWater.java
+++ src/main/java/com/algo/leet/array/ContainerWithMostWater.java
@@ -4,11 +4,27 @@ class ContainerWithMostWater {
     public static void main(String[] args) {
         int[] h = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         int[] h1 = {1, 1};
-        System.out.println(new ContainerWithMostWater().maxArea(h));
+        System.out.println(new ContainerWithMostWater().maxArea(h1));
+    }
+
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0, right = height.length - 1;
+        while(left < right) {
+            int min = Math.min(height[left], height[right]);
+            int lmax = min * (right - left);
+            maxArea = Math.max(maxArea, lmax);
+            if(height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
     }
 
     //O(n * n) - Time limit error
-    public int maxArea(int[] height) {
+    public int maxArea1(int[] height) {
         int maxArea = 0;
         for (int i = 0; i < height.length; i++) {
             int lmax = 0;
